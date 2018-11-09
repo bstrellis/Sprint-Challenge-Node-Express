e4f1c5d617459e7cfba8ddd13b8ecc8a840c2f42
Brandon Strellis <bstrellis@gmail.com>
2018-11-09 13:52:45 -0500

    Refactored projectsRouter to include conditional checks for valid ids in query string.

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -13,21 +13,24 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
+    const id = req.params.id;
     try {
-        const id = req.params.id;
         const project = await projectModel.get(id);
-        // refactor this to screen for valid id
         res.status(200).json(project);
     } catch(err) {
-        res.status(404).json({ message: err });
+        res.status(404).json({ message: `Project ${id} does not exist.` });
     }
 });
 
 router.get('/projectActions/:projectId', async (req, res) => {
+    const projectId = req.params.projectId;
     try {
-        const projectId = req.params.projectId;
         const actionsOfProject = await projectModel.getProjectActions(projectId);
-        res.status(200).json(actionsOfProject);
+        if (actionsOfProject.length !== 0) {
+            res.status(200).json(actionsOfProject);
+        } else {
+            res.status(404).json({ message: `There are no actions for project ${projectId}`})
+        }
     } catch(err) {
         res.status(418).json({ message: err });
     }
@@ -44,22 +47,29 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
+    const id = req.params.id;
     try {
-        const id = req.params.id;
         const projectUpdate = req.body;
         const updatedProject = await projectModel.update(id, projectUpdate);
-        // refactor to screen for valid id
-        res.status(200).json(updatedProject);
+        if (updatedProject !== null) {
+            res.status(200).json(updatedProject);
+        } else {
+            res.status(418).json(`Project ${id} does not exist and therefore cannot be updated.`);
+        }
     } catch(err) {
         res.status(500).json({ message: err });
     }
 });
 
 router.delete('/:id', async (req, res) => {
+    const id = req.params.id;
     try {
-        const id = req.params.id;
         const deleteCount = await projectModel.remove(id);
-        res.status(200).json({deleteCount, id});
+        if (deleteCount !== 0) {
+            res.status(200).json({deleteCount, id});
+        } else {
+            res.status(501).json({ message: `Project ${id} does not exist and therefore cannot be deleted.`})
+        }
     } catch(err) {
         res.status(404).json({ message: err });
     }
